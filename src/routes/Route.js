3d4c3b9f51acb2aac208dbd48df656ed0f75fe22
Victor Biasibetti <victorbiasibetti7@gmail.com>
2019-11-12 22:15:39 -0300

    adição de plugins e estilização de paginas de signIn e signUp

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import AuthLayout from '~/pages/_Layout/auth';
+import DefaultLayout from '~/pages/_Layout/default';
+
 export default function RouterWrapper({
   component: Component,
   isPrivate,
@@ -13,7 +16,18 @@ export default function RouterWrapper({
 
   if (signed && !isPrivate) return <Redirect to="/dasboard" />;
 
-  return <Route {...rest} component={Component} />;
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouterWrapper.propTypes = {
