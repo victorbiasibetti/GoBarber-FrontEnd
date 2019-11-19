7678b4da61806a69369e6ba94066856669f1624d
Victor Biasibetti <victorbiasibetti7@gmail.com>
2019-11-18 22:14:16 -0300

    tratamento de erro ao logar na aplicação

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -2,30 +2,34 @@ import { all, takeLatest, call, put } from 'redux-saga/effects';
 
 import api from '~/services/api';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 import history from '~/services/history';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  console.tron.log('chego aqui');
+    console.tron.log('chego aqui');
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('Usuário não é um prestador de serviço');
-    return;
+    if (!user.provider) {
+      console.tron.error('Usuário não é um prestador de serviço');
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
